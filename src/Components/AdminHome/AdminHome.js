bf3b62272d0306deb8d08362dbe7914128b1f757
Muhid Hossain <muhidhossain7@gmail.com>
2020-10-28 23:06:47 +0600

    bug fixed

--- src/Components/AdminHome/AdminHome.js
+++ src/Components/AdminHome/AdminHome.js
@@ -1,61 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AdminHome.css'
 import { useForm } from 'react-hook-form';
 import { Button } from '@material-ui/core';
 
 const AdminHome = () => {
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = data => console.log(data);
+    const [allBooks, setAllBooks] = useState(null);
+    const { register, handleSubmit, errors, reset } = useForm();
+
+     const onSubmit = (data) => {
+        const allBooks = data
+        console.log(allBooks);
+        fetch("https://boiwala.herokuapp.com/addBooks", {
+            method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(allBooks)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setAllBooks(data)
+                reset()
+            })
+    };
 
     return (
         <div className="adminHome">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input name="image" placeholder="Image" ref={register({ required: true })} />
+                <input name="image" placeholder="Image URL*" ref={register({ required: true })} />
                 <br/>
-                {errors.image && <span className="error">This field is required</span>}
+                {errors.image && <span className="error">Image URL is required*</span>}
                 <br/>
-                <input name="title" placeholder="Title" ref={register({ required: true })} />
+                <input name="title" placeholder="Title*" ref={register({ required: true })} />
                 <br/>
-                {errors.title && <span className="error">This field is required</span>}
+                {errors.title && <span className="error">Title is required*</span>}
                 <br/>
-                <input name="author" placeholder="Author" ref={register({ required: true })} />
+                <input name="author" placeholder="Author*" ref={register({ required: true })} />
                 <br/>
-                {errors.author && <span className="error">This field is required</span>}
+                {errors.author && <span className="error">Author is required*</span>}
                 <br/>
-                <input name="category" placeholder="Category" ref={register({ required: true })} />
+                <input name="category" placeholder="Category*" ref={register({ required: true })} />
                 <br/>
-                {errors.category && <span className="error">This field is required</span>}
+                {errors.category && <span className="error">Category is required*</span>}
                 <br/>
-                <input name="publisher" placeholder="Publisher" ref={register({ required: true })} />
+                <input name="publisher" placeholder="Publisher*" ref={register({ required: true })} />
                 <br/>
-                {errors.publisher && <span className="error">This field is required</span>}
+                {errors.publisher && <span className="error">Publisher is required*</span>}
                 <br/>
-                <input name="isbn" placeholder="ISBN" ref={register({ required: true })} />
+                <input name="isbn" placeholder="ISBN*" ref={register({ required: true })} />
                 <br/>
-                {errors.isbn && <span className="error">This field is required</span>}
+                {errors.isbn && <span className="error">ISBN is required*</span>}
                 <br/>
-                <input name="edition" placeholder="Edition" ref={register({ required: true })} />
+                <input name="edition" placeholder="Edition*" ref={register({ required: true })} />
                 <br/>
-                {errors.edition && <span className="error">This field is required</span>}
+                {errors.edition && <span className="error">Edition is required*</span>}
                 <br/>
-                <input name="numberOfPages" placeholder="Number of pages" ref={register({ required: true })} />
+                <input name="numberOfPages" placeholder="Number of pages*" ref={register({ required: true })} />
                 <br/>
-                {errors.numberOfPages && <span className="error">This field is required</span>}
+                {errors.numberOfPages && <span className="error">Number of pages is required*</span>}
                 <br/>
-                <input name="country" placeholder="Country" ref={register({ required: true })} />
+                <input name="country" placeholder="Country*" ref={register({ required: true })} />
                 <br/>
-                {errors.country && <span className="error">This field is required</span>}
-                <br/><input name="language" placeholder="Language" ref={register({ required: true })} />
+                {errors.country && <span className="error">Country is required*</span>}
                 <br/>
-                {errors.language && <span className="error">This field is required</span>}
+                <input name="language" placeholder="Language*" ref={register({ required: true })} />
                 <br/>
-                <input name="price" placeholder="Price" ref={register({ required: true })} />
+                {errors.language && <span className="error">This field is required*</span>}
                 <br/>
-                {errors.price && <span className="error">This field is required</span>}
+                <input name="price" placeholder="Price*" ref={register({ required: true })} />
                 <br/>
-                <input name="stock" placeholder="Stock" ref={register({ required: true })} />
+                {errors.price && <span className="error">Price is required*</span>}
                 <br/>
-                {errors.stock && <span className="error">This field is required</span>}
+                <input name="discountPrice" placeholder="Discount Price*" ref={register({ required: true })} />
+                <br/>
+                {errors.discountPrice && <span className="error">Discount Price is required*</span>}
+                <br/>
+                <input name="stock" placeholder="Stock*" ref={register({ required: true })} />
+                <br/>
+                {errors.stock && <span className="error">Stock is required*</span>}
                 <br/>
                 <Button className="submitBtn" type="submit">Submit</Button>
             </form>
